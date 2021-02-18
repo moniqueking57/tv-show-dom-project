56ab9a686a01014087661e6b124920e4920095d2
moniqueking57 <65675759+moniqueking57@users.noreply.github.com>
2021-02-18 15:40:32 +0000

    level 300 done

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const searchBox = document.getElementById("livesearch");
 // eslint-disable-next-line no-undef
 const allEpisodes = getAllEpisodes();
+const searchBox = document.getElementById("livesearch");
 const pancakes = document.createElement("span");
+const rootElem = document.getElementById("root");
+const clive = document.getElementById("pie");
 const xp = document.getElementById("chocolatePancakes");
 xp.appendChild(pancakes);
-const rootElem = document.getElementById("root");
 
 function setup() {
   makePageForEpisodes(allEpisodes);
@@ -12,23 +13,18 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   episodeList.forEach((item) => {
-    //  console.log(item);
     rootElem.insertAdjacentHTML(
       "afterbegin",
       `<div class="episode">
-                            <h2>${item.name}</h2><div>
-                            <img src="${item.image.medium}" alt="">
-                            <br>
-                            ${item.summary}
-                            S${item.season
-                              .toString()
-                              .padStart(
-                                2,
-                                0
-                              )}E${item.number.toString().padStart(2, 0)}
-                            <br>
-                            <a href=${item.url}>Check the source</a>
-                        </div>`
+      <h2>${item.name}</h2><div>
+      <img src="${item.image.medium}" alt="">
+      <br>
+      ${item.summary}
+      S${item.season.toString().padStart(2, 0)}
+      E${item.number.toString().padStart(2, 0)}
+      <br>
+      <a href=${item.url}>Check the source</a>
+    </div>`
     );
   });
 }
@@ -37,9 +33,7 @@ function makePageForEpisodes(episodeList) {
 function searchFunction(episodeList) {
   searchBox.addEventListener("input", (e) => {
     e.preventDefault();
-    // console.log(e.target)
     const searchString = e.target.value.toLowerCase();
-    // console.log(`coolee ${e.target.value}`)
     let filteredEpisodes = episodeList.filter((epis) => {
       return (
         epis.name.toLowerCase().includes(searchString) ||
@@ -47,16 +41,43 @@ function searchFunction(episodeList) {
       );
     });
     rootElem.innerHTML = "";
-    //the number of search results.
-    // pancakes.innerHTML = filteredEpisodes.length;
-    pancakes.innerHTML = `${filteredEpisodes.length}/${episodeList.length} results found.`;
-    //end the number of search results.
-
+    pancakes.innerHTML = `${filteredEpisodes.length}/${episodeList.length} results found.`; //the number of search results.
     makePageForEpisodes(filteredEpisodes);
   });
 }
-
-// eslint-disable-next-line no-undef
+//select drop down bar
+function dropDown(arr) {
+  let drops = `<option value='1' >See all episodes</option>`;
+  arr.forEach((item) => {
+    drops += `<option value="${item.id}"> 
+      S${item.season.toString().padStart(2, 0)}
+      E${item.number.toString().padStart(2, 0)}
+      ${item.name}</option>`;
+  });
+  clive.innerHTML = "";
+  clive.insertAdjacentHTML("afterbegin", drops);
+}
+dropDown(allEpisodes);
 searchFunction(allEpisodes);
+
+function selectMenu(episodeList) {
+  clive.addEventListener("change", (e) => {
+    e.preventDefault();
+    const searchId = +e.target.value;
+    let filteredList = [];
+    console.log(searchId);
+    if (searchId === 1) {
+      filteredList = episodeList;
+    } else {
+      filteredList = episodeList.filter((epis) => {
+        return epis.id === searchId;
+      });
+    }
+    console.log(searchId);
+    rootElem.innerHTML = "";
+    makePageForEpisodes(filteredList);
+  });
+}
+selectMenu(allEpisodes);
 
 window.onload = setup;
