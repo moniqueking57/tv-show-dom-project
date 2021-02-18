5d5e7118e12b8277d512628ff7e8efe6a61e8f4d
moniqueking57 <65675759+moniqueking57@users.noreply.github.com>
2021-02-18 09:59:08 +0000

    chocolate pancake update

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const searchBox = document.getElementById("livesearch");
+// eslint-disable-next-line no-undef
 const allEpisodes = getAllEpisodes();
+const pancakes = document.createElement("span");
+const xp = document.getElementById("chocolatePancakes");
+xp.appendChild(pancakes);
 const rootElem = document.getElementById("root");
 
 function setup() {
@@ -7,54 +11,52 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
- episodeList.forEach((item) =>{
-  //  console.log(item);
-  rootElem.insertAdjacentHTML("afterbegin", `<div class="episode">
+  episodeList.forEach((item) => {
+    //  console.log(item);
+    rootElem.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="episode">
                             <h2>${item.name}</h2><div>
                             <img src="${item.image.medium}" alt="">
                             <br>
                             ${item.summary}
-                            S${item.season.toString().padStart(2, 0)}E${item.number.toString().padStart(2, 0)}
+                            S${item.season
+                              .toString()
+                              .padStart(
+                                2,
+                                0
+                              )}E${item.number.toString().padStart(2, 0)}
                             <br>
                             <a href=${item.url}>Check the source</a>
-                        </div>`)
- })
-};
-
-
-
-
-function searchFunction(episodeList){
-  searchBox.addEventListener('keypress', (e) => {
-    const searchString = e.target.value.toLowerCase();
-
-  let filteredEpisodes = episodeList.filter((epis) => {
-        return (
-            epis.name.toLowerCase().includes(searchString)      
-        );   
-    });
-   makePageForEpisodes(filteredEpisodes);
-}); 
+                        </div>`
+    );
+  });
 }
 
+//creates search box and functionality
+function searchFunction(episodeList) {
+  searchBox.addEventListener("input", (e) => {
+    e.preventDefault();
+    // console.log(e.target)
+    const searchString = e.target.value.toLowerCase();
+    // console.log(`coolee ${e.target.value}`)
+    let filteredEpisodes = episodeList.filter((epis) => {
+      return (
+        epis.name.toLowerCase().includes(searchString) ||
+        epis.summary.toLowerCase().includes(searchString)
+      );
+    });
+    rootElem.innerHTML = "";
+    //the number of search results.
+    // pancakes.innerHTML = filteredEpisodes.length;
+    pancakes.innerHTML = `${filteredEpisodes.length}/${episodeList.length} results found.`;
+    //end the number of search results.
+
+    makePageForEpisodes(filteredEpisodes);
+  });
+}
+
+// eslint-disable-next-line no-undef
 searchFunction(allEpisodes);
-
-// fetch('shows.js')
-//   .then(response => response.json())
-//   .then(searching => {
-//   createSearchBar(searching)
-//   episodeLists = searching
-// })
-// let episodeLists = []
-
-// searchBox.addEventListener('input', event => {
-//   event.preventDefault()
-//   const searchLower = event.target.value.toLowerCase()
-//   let searchResult = episodeLists.filter(episodeList => {
-//   return episodeList.name.toLowerCase().includes(searchLower)
-  
-//   })
-// createSearchBar(searchResult)
-// })
 
 window.onload = setup;
