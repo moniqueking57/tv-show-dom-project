a658caa85768800133fca52e71cbd13f1f08cc42
Monique <moniqueking57@gmail.com>
2021-03-04 21:30:04 +0000

    finished 400, started 500

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
 const searchBox = document.getElementById("livesearch");
 const pancakes = document.createElement("span");
 const rootElem = document.getElementById("root");
+const showDropBar = document.getElementById("tart");
 const clive = document.getElementById("pie");
 const xp = document.getElementById("chocolatePancakes");
 xp.appendChild(pancakes);
 
 function setup() {
-  getFetch();
+  let showList = getAllShows();
+  showList.sort(function (a, b) {
+    var nameA = a.name.toUpperCase();
+    var nameB = b.name.toUpperCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
+  dropDown2(showList);
+  selectMenu2(showList);
+  makePageForShows(showList);
 }
 
-function getFetch() {
-  fetch("https://api.tvmaze.com/shows/82/episodes")
+function getFetch(showId) {
+  fetch(`https://api.tvmaze.com/shows/${showId}/episodes`)
     .then((episodes) => {
-      // changing the json object to the javascript object (.json returns promise)
       return episodes.json();
     })
-
     .then((data) => {
-      console.log(data);
       makePageForEpisodes(data);
+      makePageForShows(data);
       dropDown(data);
       searchFunction(data);
       selectMenu(data);
-      console.log("fetched");
     })
-
     .catch((err) => console.log("err", err));
 }
 
@@ -33,14 +44,34 @@ function makePageForEpisodes(episodeList) {
     rootElem.insertAdjacentHTML(
       "afterbegin",
       `<div class="episode">
-      <h2>${item.name}</h2><div>
-      <img src="${item.image.medium}" alt="">
+      <h2>${item?.name}</h2><div>
+      <img src="${item.image?.medium}" alt="">
       <br>
-      ${item.summary}
-      S${item.season.toString().padStart(2, 0)}
-      E${item.number.toString().padStart(2, 0)}
+      ${item?.summary}
+      S${item?.season.toString().padStart(2, 0)}
+      E${item?.number.toString().padStart(2, 0)}
       <br>
-      <a href=${item.url}>Check the source</a>
+      <a class="url" href=${item?.url}>Check the source</a>
+    </div>`
+    );
+  });
+}
+
+function makePageForShows(showList) {
+  showList.forEach((item) => {
+    rootElem.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="shows">
+      <h2>${item?.name}</h2><div>
+      <img src="${item.image?.medium}" alt="">
+      <br>
+      ${item?.summary}
+      <p> Runtime: ${item?.runtime}</p>
+      <p> Genre: ${item?.genres}</p>
+      <p> Status: ${item?.status}</p>
+      <p> Rating: ${item?.rating?.average}</p>
+      <br>
+      <a class="url" href=${item?.url}>Check the source</a>
     </div>`
     );
   });
@@ -60,9 +91,11 @@ function searchFunction(episodeList) {
     rootElem.innerHTML = "";
     pancakes.innerHTML = `${filteredEpisodes.length}/${episodeList.length} results found.`; //the number of search results.
     makePageForEpisodes(filteredEpisodes);
+    makePageForShows(filteredEpisodes);
   });
 }
-//select drop down bar
+
+// drop down bar episodes
 function dropDown(arr) {
   let drops = `<option value='1' >See all episodes</option>`;
   arr.forEach((item) => {
@@ -91,6 +124,39 @@ function selectMenu(episodeList) {
     console.log(searchId);
     rootElem.innerHTML = "";
     makePageForEpisodes(filteredList);
+  });
+}
+
+//dropdown for shows
+function dropDown2(arr) {
+  let drops = `<option value='1'>See all shows</option>`;
+  arr.forEach((item) => {
+    drops += `<option value="${item.id}"> 
+      ${item.name}</option>`;
+  });
+  showDropBar.innerHTML = "";
+  showDropBar.insertAdjacentHTML("afterbegin", drops);
+}
+
+function selectMenu2(showList) {
+  showDropBar.addEventListener("change", (candle) => {
+    candle.preventDefault();
+    const searching = +candle.target.value;
+    let filteredLists = [];
+    if (searching === 1) {
+      rootElem.innerHTML = "";
+      makePageForShows(showList);
+      filteredLists = showList.name;
+      return "";
+    } else {
+      filteredLists = showList.filter((show) => {
+        return show.id === searching;
+      });
+    }
+    rootElem.innerHTML = "";
+    if (candle.target.value != 1) {
+      getFetch(filteredLists[0].id);
+    }
   });
 }
 
