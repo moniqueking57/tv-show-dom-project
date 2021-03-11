e8fcfbc7ea94c9b07268ec16aaa49bd6f09f981a
Monique <moniqueking57@gmail.com>
2021-03-11 12:56:17 +0000

    all functioning now just need to change css a little

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-//The code works when I click on the button but when I click on the drop down the button
-// is still available and it gets weirrdddd!!!!!
-const searchBox = document.getElementById("livesearch");
+const searchBox = document.getElementById("livesearchShows");
+const searchBoxEp = document.getElementById("livesearchEpisodes");
 const searchForEpisodes = document.getElementById("span");
 const rootElem = document.getElementById("root");
 const showDropBar = document.getElementById("showBar");
 const episodeDropBar = document.getElementById("episodeBar");
 const xp = document.getElementById("search");
 xp.appendChild(searchForEpisodes);
-// let fetchedEpisodes;
+
+let fetchedEpisodes;
 
 function setup() {
   // eslint-disable-next-line no-undef
@@ -35,22 +35,22 @@ function getFetch(showId) {
       return episodes.json();
     })
     .then((data) => {
-      // fetchedEpisodes = data;
-      makePageForShows(data);
+      fetchedEpisodes = data;
+      console.log(data);
+      makePageForEpisodes(data);
       dropDown(data);
       searchFunction(data);
       selectMenu(data);
     })
     .catch((err) => console.log("error", err));
 }
-// function for displaying the list of episodes
+
 function makePageForEpisodes(episodeList) {
-  rootElem.innerHTML = "";
   episodeList.forEach((item) => {
     rootElem.insertAdjacentHTML(
       "afterbegin",
-      `<div class="episode">
-      <h2>${item?.name}</h2><div>
+      `<div id='episode' class="episode">
+      <h2 id="titleH2" data-value=${item?.id}>${item?.name}</h2><div>
       <img src="${item.image?.medium}" alt="">
       <br>
       <div class="summary">${item?.summary}</div>
@@ -61,14 +61,27 @@ function makePageForEpisodes(episodeList) {
     </div>`
     );
   });
+  const clickBtn = document.getElementById("episode").parentElement;
+  clickBtn.addEventListener("click", (e) => {
+    if (!e.target.dataset.value) return;
+    const episodeList2 =
+      episodeList.length !== 1 ? episodeList : fetchedEpisodes;
+    console.log(episodeList2);
+    console.log(e.target.dataset.value);
+    const query = e.target.dataset.value;
+    const episode = episodeList2.filter((item) => item.id === +query);
+    console.log(episode);
+    clickBtn.innerHTML = "";
+    makePageForEpisodes([episode[0]]);
+  });
 }
 
-//function for displaying the list of shows and the button functionality
 function makePageForShows(showList) {
+  console.log(showList);
   showList.forEach((item) => {
     rootElem.insertAdjacentHTML(
       "afterbegin",
-      ` <div id="hideInfo" style="display:block">
+      `<div id="hideInfo" style="display:block">
       <h2>${item?.name}</h2><div>
       <div class="shows">
       <div class="flex-container">
@@ -81,32 +94,23 @@ function makePageForShows(showList) {
       <br> Rating: ${item?.rating?.average}
       <br>
       <a class="url" href=${item?.url}>Check the source</a></p>
-      <button class="btn" id="episode-btn" style="flex">View episodes</button>
+      <button  class="btn" id="episode-btn" style="flex">View episodes</button>
       </div>
       </div>
     </div>`
     );
-    // I want the show list in alphabetical order
-    // showList.sort(function (a, b) {
-    //   let nameA = a.name.toUpperCase();
-    //   let nameB = b.name.toUpperCase();
-    //   if (nameA < nameB) {
-    //     return -1;
-    //   }
-    //   if (nameA > nameB) {
-    //     return 1;
-    //   }
-    //   return 0;
-    // });
     const clickBtn = document.getElementById("episode-btn");
     clickBtn.addEventListener("click", (e) => {
       e.preventDefault();
+      console.log(item);
       fetch(`https://api.tvmaze.com/shows/${item.id}/episodes`)
         .then((episodes) => {
           return episodes.json();
         })
         .then((data) => {
+          fetchedEpisodes = data;
           makePageForEpisodes(data);
+          // rootElem.style.display = "none";
           console.log("x", data);
         });
     });
@@ -115,7 +119,7 @@ function makePageForShows(showList) {
 
 //creates search box and functionality for episodes
 function searchFunction(episodeList) {
-  searchBox.addEventListener("input", (e) => {
+  searchBoxEp.addEventListener("input", (e) => {
     e.preventDefault();
     const searchString = e.target.value.toLowerCase();
     let filteredEpisodes = episodeList.filter((epis) => {
@@ -191,11 +195,14 @@ function dropDown2(arr) {
 
 function selectMenu2(showList) {
   showDropBar.addEventListener("change", (e) => {
+    searchBoxEp.classList.remove("hiddenClass");
     e.preventDefault();
     const searching = +e.target.value;
     let filteredLists = [];
     if (searching === 1) {
+      console.log("===");
       rootElem.innerHTML = "";
+      console.log(showList);
       makePageForShows(showList);
       filteredLists = showList.name;
       return "";
@@ -205,7 +212,9 @@ function selectMenu2(showList) {
       });
     }
     rootElem.innerHTML = "";
-    if (e.target.value != 1) {
+    if (e.target.value !== 1) {
+      console.log("!==");
+      console.log(filteredLists);
       getFetch(filteredLists[0].id);
     }
   });
